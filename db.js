9239705ee8e5efd1feafdcf472177058cff0abce
Juan Carlos Herrera <jherreras5@miumg.edu.gt>
2024-10-14 17:04:43 -0600

    subido

--- db.js
+++ db.js
@@ -1,10 +1,11 @@
 const mysql = require('mysql');
 
+// Configurar la conexión usando variables de entorno o valores predeterminados
 const connection = mysql.createConnection({
-  host: '34.28.198.131',  // IP pública de tu base de datos en Google Cloud SQL
-  user: 'root',           // Usuario correcto
-  password: 'Juancarlos123',  // Contraseña correcta
-  database: 'sanarate_fc'  // Nombre de la base de datos correcta
+  host: process.env.DB_HOST || '34.28.198.131',  // IP pública de tu base de datos en Google Cloud SQL
+  user: process.env.DB_USER || 'root',           // Usuario de la base de datos
+  password: process.env.DB_PASSWORD || 'Juancarlos123',  // Contraseña de la base de datos
+  database: process.env.DB_NAME || 'sanarate_fc'  // Nombre de la base de datos
 });
 
 connection.connect((err) => {
@@ -14,5 +15,3 @@ connection.connect((err) => {
   }
   console.log('Conectado a la base de datos en Google Cloud SQL');
 });
-
-module.exports = connection;
